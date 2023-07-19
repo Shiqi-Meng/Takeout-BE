827b829a7ab59f2a4f37ee857009871e5e830726
Shiqi-Meng <2579553820@qq.com>
2023-07-19 17:53:20 +0800

    增加功能：员工分页查询

--- sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
+++ sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
@@ -46,8 +46,7 @@ public class JwtTokenAdminInterceptor implements HandlerInterceptor {
         String token = request.getHeader(jwtProperties.getAdminTokenName());
 
         //2、校验令牌
-        try {
-            log.info("jwt校验:{}", token);
+        try {log.info("jwt校验:{}", token);
             Claims claims = JwtUtil.parseJWT(jwtProperties.getAdminSecretKey(), token);
             Long empId = Long.valueOf(claims.get(JwtClaimsConstant.EMP_ID).toString());
             log.info("当前员工id：", empId);
